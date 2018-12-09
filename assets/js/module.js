1a532f00fb713eeefeb65efaf6af21b81db47414
Maftunakhon <maftunakhon@gmail.com>
2018-12-09 12:44:46 +0900

    Тест

---
+++ assets/js/module.js
@@ -0,0 +1,143 @@
+jQuery(document).on('ready', function ($) {
+    "use strict";
+
+    /*--------------------------
+        STICKY MAINMENU
+    ---------------------------*/
+    $("#mainmenu-area").sticky({
+        topSpacing: 0
+    });
+
+    /*---------------------------
+        SMOOTH SCROLL
+    -----------------------------*/
+    $('ul#nav li a[href^="#"], a.navbar-brand, a.scrolltotop').on('click', function (event) {
+        var id = $(this).attr("href");
+        var offset = 60;
+        var target = $(id).offset().top - offset;
+        $('html, body').animate({
+            scrollTop: target
+        }, 1500, "easeInOutExpo");
+        event.preventDefault();
+    });
+
+
+    /*----------------------------
+        MOBILE & DROPDOWN MENU
+    ------------------------------*/
+    jQuery('.stellarnav').stellarNav({
+        theme: 'dark',
+        breakpoint: 900,
+    });
+
+
+    /*----------------------------
+        SCROLL TO TOP
+    ------------------------------*/
+    $(window).scroll(function () {
+        var $totalHeight = $(window).scrollTop();
+        var $scrollToTop = $(".scrolltotop");
+        if ($totalHeight > 300) {
+            $(".scrolltotop").fadeIn();
+        } else {
+            $(".scrolltotop").fadeOut();
+        }
+
+        if ($totalHeight + $(window).height() === $(document).height()) {
+            $scrollToTop.css("bottom", "90px");
+        } else {
+            $scrollToTop.css("bottom", "20px");
+        }
+    });
+
+    /*--------------------------
+       PARALLAX BACKGROUND
+    ----------------------------*/
+    $(window).stellar({
+        responsive: true,
+        positionProperty: 'position',
+        horizontalScrolling: false
+    });
+
+
+    /*---------------------------
+	    HOME SLIDER
+	-----------------------------*/
+    var $homeSlider = $('.welcome-slider-area');
+    $homeSlider.owlCarousel({
+        merge: true,
+        margin: 0,
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+            600: {
+                items: 1
+            },
+            1000: {
+                items: 1
+            },
+            1200: {
+                items: 1
+            }
+        }
+    });
+
+    /*------------------------------
+        VIDEO POPUP
+    --------------------------------*/
+    var $videoModal = $(".video-area-popup");
+    $videoModal.modalVideo({
+        channel: 'youtube'
+    });
+
+
+    /*---------------------------
+        MICHIMP INTEGRATION
+    -----------------------------*/
+    $('#mc-form').ajaxChimp({
+        url: 'http://intimissibd.us14.list-manage.com/subscribe/post?u=a77a312486b6e42518623c58a&amp;id=4af1f9af4c', //Set Your Mailchamp URL
+        callback: function (resp) {
+            if (resp.result === 'success') {
+                $('.subscriber-form input, .subscriber-form button').hide();
+            }
+        }
+    });
+
+
+
+    /*--------------------------
+        ACTIVE WOW JS
+    ----------------------------*/
+    new WOW().init();
+
+    /*---------------------------
+        PLACEHOLDER ANIMATION
+    ----------------------------*/
+    Placeholdem(document.querySelectorAll('[placeholder]'));
+
+}(jQuery));
+
+/*------------------------------
+    AUDIO
+--------------------------------*/
+
+var wavesurfer = WaveSurfer.create({
+    container: '#module-audio',
+    waveColor: 'violet',
+    progressColor: 'purple'
+});
+
+function play(){
+    wavesurfer.load("assets/audio/Mohsen Yeganeh - Behet Ghol Midam ( I promise you ).mp3")
+};
+
+jQuery(window).on('load', function () {
+    "use strict";
+    /*--------------------------
+        PRE LOADER
+    ----------------------------*/
+    $(".preeloader").fadeOut(1000);
+
+});
